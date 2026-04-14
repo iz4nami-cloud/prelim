ccf81a31aff47a75a8544bf921a31779453a0480
nelson <marknelsonlayug04@gmail.com>
2026-04-14 06:10:37 -0700

    read

--- prelimexam.py
+++ prelimexam.py
@@ -1,43 +1,6 @@
-def get_price_category(price):
-    
-    if price < 50:
-        return "Budget"
-    elif price < 200:
-        return "Mid-range"
-    else:
-        return "Premium"
+file_path = "C:\\prelim\\read.txt"
 
+with open(file_path, 'r') as file:
+    content = file.read()
 
-def check_stock_level(stock):
-    
-    if stock >= 10:
-        return "OK"
-    else:
-        return "LOW STOCK - Reorder needed"
-
-
-def save_product(name, price, category, stock, stock_status):
-    
-    with open("inventory.txt", "a") as file:
-        file.write(f"Product: {name} | Price: {price:.2f} | Category: {category} | Stock: {stock} | Status: {stock_status}\n")
-
-
-
-while True:
-    name = input("Enter product name: ")
-    price = float(input("Enter product price (in pesos): "))
-    stock = int(input("Enter quantity in stock: "))
-
-    category = get_price_category(price)
-    stock_status = check_stock_level(stock)
-
-    print("\nPrice Category:", category)
-    print("Stock Status:", stock_status)
-
-    save_product(name, price, category, stock, stock_status)
-    print("Product saved to inventory!\n")
-
-    choice = input("Add another product? (yes/no): ").lower()
-    if choice != "yes":
-        print("Inventory update complete. Goodbye!")
-        break
+print(content)
